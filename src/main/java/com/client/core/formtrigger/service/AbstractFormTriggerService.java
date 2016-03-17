5825600d121c249d76eb93585af7dfbd0b15a5d5
John Sullivan <john.sullivan@bullhorn.com>
2016-03-17 08:57:36 -0400

    Updating form trigger service constructor

--- src/main/java/com/client/core/formtrigger/service/AbstractFormTriggerService.java
+++ src/main/java/com/client/core/formtrigger/service/AbstractFormTriggerService.java
@@ -2,24 +2,41 @@ package com.client.core.formtrigger.service;
 
 import java.util.Locale;
 
+import org.apache.log4j.Logger;
 import org.springframework.context.MessageSource;
 import org.springframework.context.NoSuchMessageException;
 
+import com.client.core.AppContext;
+
 public class AbstractFormTriggerService {
 
-	protected final MessageSource validationMessages;
+	private final MessageSource validationMessages;
+    private final Logger log;
 
-	public AbstractFormTriggerService(MessageSource validationMessages) {
-		super();
-		this.validationMessages = validationMessages;
-	}
+    public AbstractFormTriggerService(MessageSource validationMessages) {
+        this.validationMessages = validationMessages;
+        this.log = Logger.getLogger(getClass());
+    }
 
-	protected String getMessageUsingKey(String key) throws NoSuchMessageException {
+    public AbstractFormTriggerService() {
+        this.validationMessages = AppContext.getApplicationContext().getBean("messageSource", MessageSource.class);
+        this.log = Logger.getLogger(getClass());
+    }
+
+    protected String getMessageUsingKey(String key) throws NoSuchMessageException {
         return getMessageUsingKey(key, new Object[] {});
 	}
 	
     protected String getMessageUsingKey(String key, Object[] args) throws NoSuchMessageException {
         return validationMessages.getMessage(key, args, Locale.US);
+    }
+
+    protected Logger getLog() {
+        return log;
+    }
+
+    protected MessageSource getValidationMessages() {
+        return validationMessages;
     }
 
 }
