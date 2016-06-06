d59d9056703c02747aac8e89c560dac273a8b3c2
John Sullivan <john.sullivan@bullhorn.com>
2016-06-06 08:55:21 -0400

    First work for form script example

--- src/main/webapp/javascript/formscript/main/clientcorporation.js
+++ src/main/webapp/javascript/formscript/main/clientcorporation.js
@@ -1,7 +1,21 @@
 define([], function() {
     $(function() {
-        var id = window.parent.cache.get('AppData').userInformation.userData.id;
+        var engage = {
+            adminUserTypes: '${client.admin.usertypes}'.split(','),
 
-        console.log(id);
+            isUserAdmin: function() {
+                var userTypeID = window.parent.cache.get('AppData').userInformation.userData.userType.id;
+
+                return this.adminUserTypes.indexOf(userTypeID) >= 0;
+            },
+
+            disableCreditCheck: function() {
+                $('[name="customText2"]').find('option:not(:selected)').prop('disabled', true);
+            }
+        };
+
+        if(!engage.isUserAdmin()) {
+            engage.disableCreditCheck();
+        }
     });
 });
