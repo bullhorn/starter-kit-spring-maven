784dfe15d8e0050aa4945770265049967661ad7c
hiqbal <hiqbal@bullhorn.com>
2017-06-04 18:25:29 -0400

    updated service for custom tab

--- src/main/java/com/client/custom/service/frame/impl/StandardFrameService.java
+++ src/main/java/com/client/custom/service/frame/impl/StandardFrameService.java
@@ -1,7 +1,17 @@
 package com.client.custom.service.frame.impl;
 
+import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
+import com.bullhornsdk.data.exception.RestApiException;
+import com.bullhornsdk.data.model.entity.core.standard.CorporateUser;
+import com.bullhornsdk.data.model.entity.core.standard.JobOrder;
+import com.bullhornsdk.data.model.parameter.QueryParams;
+import com.bullhornsdk.data.model.parameter.standard.ParamFactory;
+import com.google.common.collect.Maps;
+import org.apache.commons.lang.StringUtils;
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -21,6 +31,7 @@ public class StandardFrameService implements FrameService {
     private static final Set<String> JOB_FIELDS = Sets.newHashSet("title", "id");
 
     private final BullhornData bullhornData;
+    private final Logger logger = Logger.getLogger(StandardFrameService.class);
 
     @Autowired
     public StandardFrameService(BullhornData bullhornData) {
@@ -29,7 +40,35 @@ public class StandardFrameService implements FrameService {
 
     @Override
     public FrameData getFrameData(Integer corporateUserID, String entityType, Set<Integer> entityIDs) {
-        return null;
+
+        FrameData frameData = new FrameData();
+
+        try {
+            CorporateUser corporateUser = bullhornData.findEntity(CorporateUser.class, corporateUserID, CORPORATE_USER_FIELDS);
+            frameData.setCorporateUserID(corporateUserID);
+            frameData.setCorporateUserName(corporateUser.getName());
+            frameData.setCorporateUserPrimaryDepartment(corporateUser.getPrimaryDepartment().getName());
+
+            String where = "id IN (" + StringUtils.join(entityIDs, ",") + ")";
+
+            QueryParams queryParams = ParamFactory.queryParams();
+            queryParams.setCount(500);
+
+            List<JobOrder> jobs = bullhornData.queryForList(JobOrder.class, where, JOB_FIELDS, queryParams);
+
+            Map<Integer, String> nameIdMap = Maps.newHashMap();
+
+            jobs.stream().forEach(a -> {
+                nameIdMap.put(a.getId(), a.getTitle());
+            });
+
+            frameData.setEntityType("JobOrder");
+            frameData.setNameIdMap(nameIdMap);
+        } catch(RestApiException e) {
+            logger.error(e.getMessage(), e);
+        }
+
+        return frameData;
     }
 
 }
