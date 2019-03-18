dee68ec0da7ae6b2063e6c3a61a9bf056aa3d62a
kcable <kcable@bullhorn.com>
2019-03-18 09:39:49 -0400

    adding special field logic for note reference fields on rest trigger add/edit

--- src/main/java/com/client/core/resttrigger/model/helper/impl/NoteRestTriggerHelper.java
+++ src/main/java/com/client/core/resttrigger/model/helper/impl/NoteRestTriggerHelper.java
@@ -1,12 +1,16 @@
 package com.client.core.resttrigger.model.helper.impl;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
 import com.bullhornsdk.data.api.BullhornData;
 import com.bullhornsdk.data.model.entity.core.standard.Note;
+import com.bullhornsdk.data.model.entity.embedded.OneToMany;
 import com.client.core.base.model.helper.impl.NoteTriggerHelper;
 import com.client.core.base.util.TriggerUtil;
+import com.client.core.resttrigger.model.enums.NoteReferenceFields;
 import com.client.core.resttrigger.model.helper.RestTriggerHelper;
 
 /**
@@ -31,7 +35,9 @@ public class NoteRestTriggerHelper extends NoteTriggerHelper implements RestTrig
 	@Override
 	public Note getNewEntity() {
 		if(newEntity == null) {
-			setNewEntity(TriggerUtil.populateEntity(entityID, Note.class, valuesChanged, Note::new));
+			Note newEntity = TriggerUtil.populateEntity(entityID, Note.class, valuesChanged, Note::new);
+			applySpecialFieldRules(newEntity);
+			setNewEntity(newEntity);
 		}
 
 		return newEntity;
@@ -50,5 +56,24 @@ public class NoteRestTriggerHelper extends NoteTriggerHelper implements RestTrig
 	@Override
 	public Map<String, Object> getValuesChanged() {
 		return valuesChanged;
+	}
+
+	private void applySpecialFieldRules(Note note){
+		try{
+			Arrays.asList(NoteReferenceFields.values()).parallelStream().forEach(referenceField -> {
+				if (valuesChanged.get(referenceField.getFieldName()) != null) {
+					OneToMany references;
+					if (entityID > 0) {
+						references = TriggerUtil.convertReplaceAllToEntityOneToMany((Map<String, List<Integer>>) valuesChanged.get(referenceField.getFieldName()), referenceField.getConstructor());
+					} else {
+						references = TriggerUtil.convertIdListToEntityOneToMany((List<Map<String, Integer>>) valuesChanged.get(referenceField.getFieldName()), referenceField.getConstructor());
+					}
+
+					referenceField.getSetReference().accept(note, references);
+				}
+			});
+		}catch(Exception e){
+			log.error("Error applying special field rules for note");
+		}
 	}
 }
