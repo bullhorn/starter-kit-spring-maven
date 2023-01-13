fb855b32049e70a6bf20298356bc963eb0268af6
Trevor Swartz <trevor.swartz@bullhorn.com>
2023-01-13 16:35:52 -0500

    Chaning applicable functions to lambdas and updating logging to log4j2 for classes not using it

--- src/main/java/com/client/ServletInitializer.java
+++ src/main/java/com/client/ServletInitializer.java
@@ -1,6 +1,5 @@
 package com.client;
 
-import com.client.StarterKitSpringMaven;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
