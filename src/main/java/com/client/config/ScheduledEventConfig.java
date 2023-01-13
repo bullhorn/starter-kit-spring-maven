c4f3422b01fab0e79a454b12ef91f5212aac62e8
Trevor Swartz <trevor.swartz@bullhorn.com>
2023-01-13 15:17:13 -0500

    Upgrading version of pom.xml and adding in a basic scheduled event config

--- src/main/java/com/client/config/ScheduledEventConfig.java
+++ src/main/java/com/client/config/ScheduledEventConfig.java
@@ -4,15 +4,8 @@ import com.bullhornsdk.data.api.BullhornData;
 import com.client.ApplicationSettings;
 import com.client.core.scheduledtasks.ScheduledEventProcessing;
 import com.client.core.scheduledtasks.service.EventWorkflowFactory;
-import com.client.core.scheduledtasks.workflow.node.impl.AbstractEventTask;
-import com.client.custom.scheduledtasks.TestTask;
 import org.quartz.CronTrigger;
-import org.quartz.JobBuilder;
-import org.quartz.JobDetail;
-import org.quartz.Trigger;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.quartz.CronTriggerFactoryBean;
@@ -22,10 +15,11 @@ import org.springframework.scheduling.quartz.SchedulerFactoryBean;
 import java.text.ParseException;
 import java.util.List;
 
+/**
+ * TODO: The scheduled action configured will fail on first startup
+ */
 @Configuration
 public class ScheduledEventConfig {
-
-    private List<AbstractEventTask> eventTasks;
 
     private BullhornData bullhornData;
 
@@ -33,91 +27,51 @@ public class ScheduledEventConfig {
 
     private EventWorkflowFactory eventWorkflowFactory;
 
-    private ApplicationContext applicationContext;
-
-    @Value("${testTaskOneCronExpression}")
-    private String testTaskOneCronExpression;
-
-    @Value("${testTaskTwoCronExpression}")
-    private String testTaskTwoCronExpression;
-
-    ScheduledEventConfig(List<AbstractEventTask> eventTasks,
-                         BullhornData bullhornData,
+    ScheduledEventConfig(BullhornData bullhornData,
                          ApplicationSettings appSettings,
-                         EventWorkflowFactory eventWorkflowFactory,
-                         ApplicationContext applicationContext) {
-        this.eventTasks = eventTasks;
+                         EventWorkflowFactory eventWorkflowFactory) {
         this.bullhornData = bullhornData;
         this.appSettings = appSettings;
         this.eventWorkflowFactory = eventWorkflowFactory;
-        this.applicationContext = applicationContext;
     }
 
     @Bean
-    SchedulerFactoryBean mainScheduler(@Qualifier("testTaskTrigger") CronTrigger testTaskTrigger, @Qualifier("testTaskTwoTrigger") CronTrigger testTaskTwoTrigger) {
+    SchedulerFactoryBean mainScheduler(List<CronTrigger> triggers) {
         SchedulerFactoryBean mainScheduler = new SchedulerFactoryBean();
         mainScheduler.setOverwriteExistingJobs(true);
         mainScheduler.setAutoStartup(true);
 
-        mainScheduler.setTriggers(
-                testTaskTrigger,
-                testTaskTwoTrigger
-        );
+        mainScheduler.setTriggers(triggers.toArray(new CronTrigger[triggers.size()]));
 
         return mainScheduler;
     }
 
-    @Bean("testTaskTrigger")
-    public CronTrigger factoryBeanTestTaskOne(@Qualifier("jobDetailTestTaskOne") MethodInvokingJobDetailFactoryBean jobDetail) throws ParseException {
+    @Bean
+    public CronTrigger standardCronTriggerBean(
+            MethodInvokingJobDetailFactoryBean standardFactoryBean,
+            @Value("${standardCronExpression}") String cronExpression) throws ParseException {
+
         CronTriggerFactoryBean cronTriggerFactoryBean = new CronTriggerFactoryBean();
-        cronTriggerFactoryBean.setCronExpression(this.testTaskOneCronExpression);
-        cronTriggerFactoryBean.setStartDelay(3000);
-        cronTriggerFactoryBean.setJobDetail(jobDetail.getObject());
+        cronTriggerFactoryBean.setCronExpression(cronExpression);
+        cronTriggerFactoryBean.setJobDetail(standardFactoryBean.getObject());
         cronTriggerFactoryBean.afterPropertiesSet();
 
         return cronTriggerFactoryBean.getObject();
     }
 
-    @Bean("testTaskTwoTrigger")
-    public CronTrigger factoryBeanTestTaskTwo(@Qualifier("jobDetailTestTaskTwo") MethodInvokingJobDetailFactoryBean jobDetail) throws ParseException {
-        CronTriggerFactoryBean cronTriggerFactoryBean = new CronTriggerFactoryBean();
-        cronTriggerFactoryBean.setCronExpression("0 0/1 * 1/1 * ? *");
-        cronTriggerFactoryBean.setStartDelay(3000);
-        cronTriggerFactoryBean.setJobDetail(jobDetail.getObject());
-        cronTriggerFactoryBean.afterPropertiesSet();
-
-        return cronTriggerFactoryBean.getObject();
-    }
-
-    @Bean("jobDetailTestTaskOne")
-    public MethodInvokingJobDetailFactoryBean createJobDetailFactoryBeanTestTask() {
+    @Bean
+    public MethodInvokingJobDetailFactoryBean standardFactoryBean() {
         MethodInvokingJobDetailFactoryBean factoryBean = new MethodInvokingJobDetailFactoryBean();
 
-        factoryBean.setTargetBeanName("scheduledProcessingTaskOne");
+        factoryBean.setTargetBeanName("standardSubscriptionScheduledEventProcessing");
         factoryBean.setTargetMethod("run");
         factoryBean.setConcurrent(true);
 
         return factoryBean;
     }
 
-    @Bean("jobDetailTestTaskTwo")
-    public MethodInvokingJobDetailFactoryBean createJobDetailFactoryBeanTestTaskTwo() {
-        MethodInvokingJobDetailFactoryBean factoryBean = new MethodInvokingJobDetailFactoryBean();
-
-        factoryBean.setTargetBeanName("scheduledProcessingTaskTwo");
-        factoryBean.setTargetMethod("run");
-        factoryBean.setConcurrent(true);
-
-        return factoryBean;
-    }
-
-    @Bean("scheduledProcessingTaskOne")
-    ScheduledEventProcessing scheduledEventProcessingTaskOne() {
-        return new ScheduledEventProcessing("add_company_file_staging", bullhornData, appSettings, eventWorkflowFactory);
-    }
-
-    @Bean("scheduledProcessingTaskTwo")
-    ScheduledEventProcessing scheduledEventProcessingTaskTwo() {
-        return new ScheduledEventProcessing("add_company_file_staging", bullhornData, appSettings, eventWorkflowFactory);
+    @Bean
+    ScheduledEventProcessing standardSubscriptionScheduledEventProcessing(@Value("${standardSubscriptionName}") String subName) {
+        return new ScheduledEventProcessing(subName, bullhornData, appSettings, eventWorkflowFactory);
     }
 }
