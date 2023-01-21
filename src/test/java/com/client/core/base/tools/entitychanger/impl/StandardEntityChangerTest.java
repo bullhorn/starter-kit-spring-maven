a8bed94fda3cbf7b5acf2ac3726d7457cdcaf421
Tomás El Fakih <tomaselfakih@gmail.com>
2023-01-20 23:40:27 -0400

    Testing enhancements
    - Improved coverage
    - Separated tests into their own, atomic methods
    - Make better use of `@Before`

--- src/test/java/com/client/core/base/tools/entitychanger/impl/StandardEntityChangerTest.java
+++ src/test/java/com/client/core/base/tools/entitychanger/impl/StandardEntityChangerTest.java
@@ -2,9 +2,12 @@ package com.client.core.base.tools.entitychanger.impl;
 
 import com.bullhornsdk.data.model.entity.core.standard.Candidate;
 import com.bullhornsdk.data.model.entity.core.standard.CorporateUser;
+import com.bullhornsdk.data.model.entity.core.type.BullhornEntity;
 import com.bullhornsdk.data.model.entity.embedded.Address;
 import com.client.core.base.tools.entitychanger.EntityChanger;
 import com.google.common.collect.ImmutableMap;
+import org.joda.time.DateTime;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.math.BigDecimal;
@@ -13,83 +16,257 @@ import java.util.Map;
 import static org.junit.Assert.*;
 
 public class StandardEntityChangerTest {
+    private Candidate candidate;
+    private Candidate emptyCandidate;
+    private TestBullhornEntity testBullhornEntity;
 
+    @Before
+    public void setUp() {
+        candidate = new Candidate(1);
+        candidate.setAddress(makeAddress());
+        candidate.setOwner(new CorporateUser(1));
+        candidate.setFirstName("Test");
+        candidate.setLastName("Tester");
+        candidate.setIsDeleted(true);
+        candidate.setDateOfBirth(new DateTime(1999, 3, 10, 10, 30));
 
-    @Test
-    public void retrieveField() {
-        EntityChanger entityChanger = new StandardEntityChanger();
-        // set up
-        Candidate candidate = new Candidate(1);
+        emptyCandidate = new Candidate();
+
+        testBullhornEntity = new TestBullhornEntity();
+    }
+
+    private Address makeAddress() {
         Address address = new Address();
         address.setAddress1("Test Address 1");
-        candidate.setFirstName("Test");
-        candidate.setAddress(address);
-        candidate.setOwner(new CorporateUser(1));
-        // assert basic field
+
+        return address;
+    }
+
+    @Test
+    public void retrievesPrimitiveType() {
+        EntityChanger entityChanger = new StandardEntityChanger();
         String firstName = entityChanger.retrieveField(candidate, "firstName", String.class);
         assertEquals("Test", firstName);
-        // assert nested field is preserved
-        String address1 = entityChanger.retrieveField(candidate, "address.address1", String.class);
-        assertEquals("Test Address 1", address1);
-        // assert value object is cast properly
+    }
+
+    @Test
+    public void retrievesFieldsAndCastsToCorrectType() {
+        EntityChanger entityChanger = new StandardEntityChanger();
         Object object = entityChanger.retrieveField(candidate, "address", Address.class);
         assertEquals(Address.class, object.getClass());
-        // assert BullhornEntity conversion
+    }
+
+    @Test
+    public void retrievesBullhornEntityField() {
+        EntityChanger entityChanger = new StandardEntityChanger();
         CorporateUser corporateUser = entityChanger.retrieveField(candidate, "owner", CorporateUser.class);
         assertEquals((Integer) 1, corporateUser.getId());
     }
 
     @Test
-    public void setField() {
+    public void retrievesNestedField() {
         EntityChanger entityChanger = new StandardEntityChanger();
-        // set up
-        Candidate candidate = new Candidate();
-        entityChanger.setField(candidate, "firstName", "Test");
-        entityChanger.setField(candidate, "owner.id", 1);
-        entityChanger.setField(candidate, "address.address1", "Test Address");
-        // assert sets basic property
-        assertEquals("Test", candidate.getFirstName());
-        // assert sets nested bh entity
-        assertEquals((Integer) 1, candidate.getOwner().getId());
-        // assert sets nested value object
-        assertEquals("Test Address", candidate.getAddress().getAddress1());
-        // assert sets complex map
-        Map<String, Object> category = ImmutableMap.<String, Object>builder()
-                .put("id", 1)
-                .put("name", "Test Category")
-                .build();
-        entityChanger.setField(candidate, "category", category);
-        assertNotNull(candidate.getCategory());
-        assertEquals((Integer) 1, candidate.getCategory().getId());
-        assertEquals("Test Category", candidate.getCategory().getName());
-        // assert DateTime
-        // 1670355681 = Tuesday, December 6, 2022 7:41:21 PM
-        entityChanger.setField(candidate, "dateOfBirth", 1670355681000L);
-        assertEquals(2022, candidate.getDateOfBirth().getYear());
-        assertEquals(12, candidate.getDateOfBirth().getMonthOfYear());
-        assertEquals(6, candidate.getDateOfBirth().getDayOfMonth());
-        // assert Boolean
-        entityChanger.setField(candidate, "isDeleted", true );
-        assertTrue(candidate.getIsDeleted());
-        // assert BigDecimal
-        entityChanger.setField(candidate, "dayRate", 5d);
-        assertEquals(BigDecimal.valueOf(5), candidate.getDayRate());
+        String address1 = entityChanger.retrieveField(candidate, "address.address1", String.class);
+        assertEquals("Test Address 1", address1);
+    }
 
-        // assert null values
+    @Test
+    public void retrievesBoolean() {
+        EntityChanger entityChanger = new StandardEntityChanger();
+        Boolean isDeleted = entityChanger.retrieveField(candidate, "isDeleted", Boolean.class);
+        assertTrue(isDeleted);
+    }
+
+    @Test
+    public void retrievesInteger() {
+        EntityChanger entityChanger = new StandardEntityChanger();
+        Integer id = entityChanger.retrieveField(candidate, "id", Integer.class);
+        assertEquals((Integer) 1, id);
+    }
+
+    @Test
+    public void retrievesDateTime() {
+        EntityChanger entityChanger = new StandardEntityChanger();
+        DateTime dateOfBirth = entityChanger.retrieveField(candidate, "dateOfBirth", DateTime.class);
+        assertEquals(1999, dateOfBirth.getYear());
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void failsToRetrieveIfTypeIsUnsupported() {
+        EntityChanger entityChanger = new StandardEntityChanger();
+        entityChanger.retrieveField(candidate, "address", Map.class);
+    }
+
+    @Test
+    public void setsStringField() {
+        EntityChanger entityChanger = new StandardEntityChanger();
+        entityChanger.setField(emptyCandidate, "firstName", "Test");
+        assertEquals("Test", emptyCandidate.getFirstName());
+    }
+
+    @Test
+    public void setsIntegerField() {
+        EntityChanger entityChanger = new StandardEntityChanger();
+        entityChanger.setField(emptyCandidate, "id", 1);
+        assertEquals((Integer) 1, emptyCandidate.getId());
+    }
+
+    @Test
+    public void setsIntegerFieldFromString() {
+        EntityChanger entityChanger = new StandardEntityChanger();
+        entityChanger.setField(emptyCandidate, "id", 1);
+        assertEquals((Integer) 1, emptyCandidate.getId());
+    }
+
+
+    @Test
+    public void setsNestedFieldInBullhornEntity() {
+        EntityChanger entityChanger = new StandardEntityChanger();
+        entityChanger.setField(emptyCandidate, "owner.id", 1);
+        assertEquals((Integer) 1, emptyCandidate.getOwner().getId());
+    }
+
+    @Test
+    public void setsIdInBullhornEntity() {
+        EntityChanger entityChanger = new StandardEntityChanger();
+        entityChanger.setField(emptyCandidate, "owner", 2);
+        assertEquals((Integer) 2, emptyCandidate.getOwner().getId());
+    }
+
+    @Test
+    public void setsNestedFieldInValueObject() {
+        EntityChanger entityChanger = new StandardEntityChanger();
+        entityChanger.setField(emptyCandidate, "owner.id", 1);
+        entityChanger.setField(emptyCandidate, "address.address1", "Test Address");
+        assertEquals("Test Address", emptyCandidate.getAddress().getAddress1());
+    }
+
+    @Test
+    public void setsComplexMap() {
+        EntityChanger entityChanger = new StandardEntityChanger();
+        Map<String, Object> category = ImmutableMap.<String, Object>builder().put("id", 1).put("name", "Test Category").build();
+        entityChanger.setField(emptyCandidate, "category", category);
+        assertNotNull(emptyCandidate.getCategory());
+        assertEquals((Integer) 1, emptyCandidate.getCategory().getId());
+        assertEquals("Test Category", emptyCandidate.getCategory().getName());
+    }
+
+    @Test
+    public void setsDateTimeFromTimestamp() {
+        EntityChanger entityChanger = new StandardEntityChanger();
+        // 1670355681 = Tuesday, December 6, 2022 7:41:21 PM
+        entityChanger.setField(emptyCandidate, "dateOfBirth", 1670355681000L);
+        assertEquals(2022, emptyCandidate.getDateOfBirth().getYear());
+        assertEquals(12, emptyCandidate.getDateOfBirth().getMonthOfYear());
+        assertEquals(6, emptyCandidate.getDateOfBirth().getDayOfMonth());
+    }
+
+    @Test
+    public void setsBooleanField() {
+        EntityChanger entityChanger = new StandardEntityChanger();
+        entityChanger.setField(emptyCandidate, "isDeleted", true);
+        assertTrue(emptyCandidate.getIsDeleted());
+    }
+
+    @Test
+    public void setsBooleanFieldFromString() {
+        EntityChanger entityChanger = new StandardEntityChanger();
+        entityChanger.setField(emptyCandidate, "isDeleted", "true");
+        assertTrue(emptyCandidate.getIsDeleted());
+    }
+
+    @Test
+    public void setsBigDecimalField() {
+        EntityChanger entityChanger = new StandardEntityChanger();
+        entityChanger.setField(emptyCandidate, "dayRate", 5d);
+        assertEquals(BigDecimal.valueOf(5), emptyCandidate.getDayRate());
+    }
+
+    @Test
+    public void setsNullValues() {
+        EntityChanger entityChanger = new StandardEntityChanger();
         entityChanger.setField(candidate, "firstName", null);
-        entityChanger.setField(candidate, "owner.id", null);
-        entityChanger.setField(candidate, "address.address1", null);
-        entityChanger.setField(candidate, "category", null);
-        entityChanger.setField(candidate, "dateOfBirth", null);
-        entityChanger.setField(candidate, "isDeleted", null);
-        entityChanger.setField(candidate, "dayRate", null);
+        entityChanger.setField(candidate, "lastName", null);
+        entityChanger.setField(candidate, "owner", null);
+        entityChanger.setField(candidate, "address", null);
 
         assertNull(candidate.getFirstName());
-        assertNull(candidate.getOwner().getId());
+        assertNull(candidate.getLastName());
+        assertNull(candidate.getOwner());
+        assertNull(candidate.getAddress());
+    }
+
+    @Test
+    public void setsNullNestedValues() {
+        EntityChanger entityChanger = new StandardEntityChanger();
+        entityChanger.setField(candidate, "address.address1", null);
         assertNull(candidate.getAddress().getAddress1());
-        assertNull(candidate.getCategory());
-        assertNull(candidate.getDateOfBirth());
-        assertNull(candidate.getIsDeleted());
-        assertNull(candidate.getDayRate());
+    }
+
+    @Test
+    public void returnsNullIfPropertyDoesNotExist() {
+        EntityChanger entityChanger = new StandardEntityChanger();
+        Object prop = entityChanger.retrieveField(candidate, "thisPropDoesntExist");
+        assertNull(prop);
+    }
+
+    @Test
+    public void returnsNullIfNoReadMethod() {
+        EntityChanger entityChanger = new StandardEntityChanger();
+        Object prop = entityChanger.retrieveField(testBullhornEntity, "fieldWithNoGetter");
+        assertNull(prop);
+    }
+
+    @Test(expected = RuntimeException.class)
+    public void setValueFailsIfPropertyDoesNotExist() {
+        EntityChanger entityChanger = new StandardEntityChanger();
+        entityChanger.setField(candidate, "thisPropertyDoesNotExist", "customValue");
+    }
+
+    @Test(expected = RuntimeException.class)
+    public void setValueFailsIfPropertyHasNoWriteMethod() {
+        EntityChanger entityChanger = new StandardEntityChanger();
+        entityChanger.setField(testBullhornEntity, "fieldWithNoSetter", "customValue");
+    }
+
+    @Test(expected = RuntimeException.class)
+    public void setValueFailsIfIntermediaryPropertyDoesntExist() {
+        EntityChanger entityChanger = new StandardEntityChanger();
+        entityChanger.setField(testBullhornEntity, "fieldWithNoGetter.length", 50);
+    }
+
+    @SuppressWarnings("ALL")
+    static class TestBullhornEntity implements BullhornEntity {
+        private Integer id;
+        private String fieldWithNoGetter;
+        private String fieldWithNoSetter = "value";
+
+        private String invisibleProperty;
+
+        @Override
+        public Integer getId() {
+            return this.id;
+        }
+
+        @Override
+        public void setId(Integer id) {
+            this.id = id;
+        }
+
+        public void setFieldWithNoGetter(String value) {
+            fieldWithNoGetter = value;
+        }
+
+        public String getFieldWithNoSetter() {
+            return fieldWithNoSetter;
+        }
+
+        private String getInvisibleProperty() {
+            return invisibleProperty;
+        }
+
+        private void setInvisibleProperty(String invisibleProperty) {
+            this.invisibleProperty = invisibleProperty;
+        }
     }
 }
