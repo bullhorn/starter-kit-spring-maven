46002c75108b7801230991d49552df83a4fea3e6
Trevor Swartz <trevor.swartz@bullhorn.com>
2023-03-02 10:28:54 -0500

    Removing MailSettings toString because records auto-generate it

--- src/main/java/com/client/core/email/MailSettings.java
+++ src/main/java/com/client/core/email/MailSettings.java
@@ -1,9 +1,6 @@
 package com.client.core.email;
 
-import com.google.common.collect.Lists;
-import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.context.annotation.Configuration;
 
 import java.util.List;
 
@@ -21,35 +18,4 @@ public record MailSettings(
         String sender,
         String senderName,
         Boolean disabled,
-        List<String> routeToWhenDisabled) {
-
-    @Override
-    public String toString() {
-        return new StringBuilder("MailSettings {")
-                .append("\n\t\"host\": ")
-                .append("'")
-                .append(host).append('\'')
-                .append(",\n\t\"port\": ")
-                .append("'")
-                .append(port).append('\'')
-                .append(",\n\t\"apiusername\": ")
-                .append("'")
-                .append(username).append('\'')
-                .append(",\n\t\"apipassword\": ")
-                .append("'")
-                .append(password).append('\'')
-                .append(",\n\t\"sender\": ")
-                .append("'")
-                .append(sender).append('\'')
-                .append(",\n\t\"senderName\": ")
-                .append("'")
-                .append(senderName).append('\'')
-                .append(",\n\t\"disabled\": ")
-                .append(disabled)
-                .append(",\n\t\"routeToWhenDisabled\": ")
-                .append("'")
-                .append(routeToWhenDisabled).append('\'')
-                .append('}')
-                .toString();
-    }
-}
+        List<String> routeToWhenDisabled) {}
