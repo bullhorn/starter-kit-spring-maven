8781e6f275aa88c8c95ac63e83a39a9bcc4a574a
Tomás El Fakih <tomaselfakih@gmail.com>
2023-03-31 16:20:10 -0400

    Migrating entity changer from groovy to java (#183)
    * First working java17 commit
    
    * Adding the rest prefixes for the rest endpoints
    
    * Chaning applicable functions to lambdas and updating logging to log4j2 for classes not using it
    
    * Adding security tools
    
    * Adding ValidationMessages, externalizing hibernate properties, removing unused values from AppSettings
    
    * Deleting the picker folder
    
    * Fixing up pom issues + thread ID logging + notes on json converter
    
    * Adding some TODOs and making all ApplicationSettings final
    
    * Adding some java docs to bean config + changing ApplicationSettings
    
    * Changing ApplicationSettings from class to a record
    
    * Replacing Slf4j annotation with Log4j2
    
    * Removing solo standardSub config code + removing joda time + removing security files
    
    * Refactoring cron trigger configuration
    
    * Configuring beans for restTemplate + multipart resolver + locale + removing jsonconverter function
    
    * Migrate EntityChanger from groovy to java
    
    * Refactor java 9 deprecated code. Also made use of PropertyDescriptors instead of manually looking for getter/setters
    
    * Enhanced error messages
    
    * Adding null assertions in Unit test
    
    * Testing enhancements
    
    - Improved coverage
    - Separated tests into their own, atomic methods
    - Make better use of `@Before`
    
    ---------
    
    Co-authored-by: Trevor Swartz <trevor.swartz@bullhorn.com>

--- src/main/java/com/client/core/base/tools/entitychanger/impl/StandardEntityChanger.java
+++ src/main/java/com/client/core/base/tools/entitychanger/impl/StandardEntityChanger.java
@@ -6,18 +6,20 @@ import com.client.core.base.tools.entitychanger.EntityChanger;
 import com.client.core.base.util.Utility;
 import lombok.extern.log4j.Log4j2;
 import org.apache.commons.lang3.ObjectUtils;
-import org.apache.commons.lang3.StringUtils;
+import org.apache.log4j.Logger;
 import org.joda.time.DateTime;
+import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
 
+import java.beans.PropertyDescriptor;
 import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
 import java.math.BigDecimal;
 import java.util.Arrays;
 import java.util.Map;
 
 @Log4j2
 @Service
+@SuppressWarnings("unchecked")
 public class StandardEntityChanger implements EntityChanger {
 
     public StandardEntityChanger() {
@@ -41,29 +43,38 @@ public class StandardEntityChanger implements EntityChanger {
         String finalField = fields[fields.length - 1];
         Object target = Arrays.stream(fields, 0, fields.length - 1).reduce(entity, (entityOrField, nextField) -> {
             try {
-                Method getter = entityOrField.getClass().getDeclaredMethod(getterMethodName(nextField));
-                Class<?> fieldType = getter.getReturnType();
-                Method setter = entityOrField.getClass().getDeclaredMethod(setterMethodName(nextField), fieldType);
-                Object maybeValue = getter.invoke(entityOrField);
-                if (maybeValue == null) {
-                    setter.invoke(entityOrField, fieldType.newInstance());
+                PropertyDescriptor propertyDescriptor = BeanUtils.getPropertyDescriptor(entityOrField.getClass(), nextField);
+                if (propertyDescriptor == null) {
+                    throw new RuntimeException("Could not find property descriptor for " + nextField);
                 }
-                return (T) getter.invoke(entityOrField);
-            } catch (IllegalAccessException | InvocationTargetException | NoSuchMethodException |
+                Class<?> fieldType = propertyDescriptor.getPropertyType();
+                if (propertyDescriptor.getReadMethod() == null) {
+                    throw new RuntimeException("Could not keep traversing - property has no read method: " + nextField);
+                }
+                Object maybeValue = propertyDescriptor.getReadMethod().invoke(entityOrField);
+                if (maybeValue == null) {
+                    propertyDescriptor.getWriteMethod().invoke(entityOrField, fieldType.getDeclaredConstructor().newInstance());
+                }
+                return (T) propertyDescriptor.getReadMethod().invoke(entityOrField);
+            } catch (IllegalAccessException |
+                     InvocationTargetException |
+                     NoSuchMethodException |
                      InstantiationException e) {
-                throw new RuntimeException(e);
+                throw new RuntimeException("Error setting property " + nextField, e);
             }
         }, (entity1, entity2) -> entity1);
         try {
-            Method getter = target.getClass().getDeclaredMethod(getterMethodName(finalField));
-            Class<?> fieldType = getter.getReturnType();
-            Method setter = target.getClass().getDeclaredMethod(setterMethodName(finalField), fieldType);
+            PropertyDescriptor propertyDescriptor = BeanUtils.getPropertyDescriptor(target.getClass(), finalField);
+            if (propertyDescriptor == null) {
+                throw new RuntimeException("Could not find property descriptor for " + finalField);
+            }
+            Class<?> fieldType = propertyDescriptor.getPropertyType();
             if (value instanceof Map) {
-                if (getter.invoke(target) == null) {
-                    setter.invoke(target, fieldType.newInstance());
+                if (propertyDescriptor.getReadMethod().invoke(target) == null) {
+                    propertyDescriptor.getWriteMethod().invoke(target, fieldType.getDeclaredConstructor().newInstance());
                 }
 
-                T nestedTarget = (T) getter.invoke(target);
+                T nestedTarget = (T) propertyDescriptor.getReadMethod().invoke(target);
                 ((Map<String, ?>) value).forEach((key, mapVal) -> {
                     try {
                         setField(nestedTarget, key, mapVal);
@@ -73,26 +84,29 @@ public class StandardEntityChanger implements EntityChanger {
                 });
             } else {
                 try {
-                    setter.invoke(target, value);
+                    propertyDescriptor.getWriteMethod().invoke(target, value);
                 } catch (IllegalAccessException | InvocationTargetException | IllegalArgumentException e) {
                     if (DateTime.class.equals(fieldType)) {
-                        setter.invoke(target, asType(value, DateTime.class));
+                        propertyDescriptor.getWriteMethod().invoke(target, asType(value, DateTime.class));
                     } else if (BullhornEntity.class.isAssignableFrom(fieldType)) {
-                        BullhornEntity bullhornEntity = (BullhornEntity) fieldType.newInstance();
+                        BullhornEntity bullhornEntity = (BullhornEntity) fieldType.getDeclaredConstructor().newInstance();
                         bullhornEntity.setId((Integer) value);
-                        setter.invoke(target, bullhornEntity);
+                        propertyDescriptor.getWriteMethod().invoke(target, bullhornEntity);
                     } else if (Integer.class.equals(fieldType)) {
-                        setter.invoke(target, asType(value, Integer.class));
+                        propertyDescriptor.getWriteMethod().invoke(target, asType(value, Integer.class));
                     } else if (BigDecimal.class.equals(fieldType)) {
-                        setter.invoke(target, asType(value, BigDecimal.class));
+                        propertyDescriptor.getWriteMethod().invoke(target, asType(value, BigDecimal.class));
                     } else if (Boolean.class.equals(fieldType)) {
-                        setter.invoke(target, asType(value, Boolean.class));
+                        propertyDescriptor.getWriteMethod().invoke(target, asType(value, Boolean.class));
                     } else {
                         log.error("Error setting field " + finalField + " to value " + value, e);
                     }
                 }
             }
-        } catch (NoSuchMethodException | IllegalAccessException | InvocationTargetException |
+        } catch (NoSuchMethodException |
+                 IllegalAccessException |
+                 InvocationTargetException |
+                 ClassCastException |
                  InstantiationException e) {
             log.error("Error setting field " + finalField + " to value " + value + ", no property " + finalField + " on " + target.getClass().getSimpleName(), e);
         }
@@ -102,28 +116,28 @@ public class StandardEntityChanger implements EntityChanger {
     private Object getValue(Object entity, String field) {
         return Arrays.stream(field.split("\\.")).reduce(entity, (entityOrField, nextField) -> {
             try {
-                Method getter = entityOrField.getClass().getDeclaredMethod(getterMethodName(nextField));
-                return getter.invoke(ObjectUtils.defaultIfNull(entityOrField, entityOrField.getClass().getDeclaredConstructor().newInstance()));
-            } catch (InvocationTargetException | IllegalAccessException | InstantiationException | NoSuchMethodException e) {
-                log.error("Could not get Field" + nextField + ".", e);
+                PropertyDescriptor propertyDescriptor = BeanUtils.getPropertyDescriptor(entityOrField.getClass(), nextField);
+                if (propertyDescriptor == null || propertyDescriptor.getReadMethod() == null) {
+                    String message = propertyDescriptor == null
+                            ? "Could not find a property descriptor for " + nextField
+                            : "Could not find read method for property descriptor for " + nextField;
+                    log.error(message);
+                    return null;
+                }
+                return propertyDescriptor.getReadMethod().invoke(ObjectUtils.defaultIfNull(entityOrField, entityOrField.getClass().getDeclaredConstructor().newInstance()));
+            } catch (InvocationTargetException | IllegalAccessException | InstantiationException |
+                     NoSuchMethodException e) {
+                log.error("Could not get Field " + nextField + ".", e);
                 return null;
             }
         }, (entity1, entity2) -> entity1);
-    }
-
-    private String getterMethodName(String fieldName) {
-        return "get" + StringUtils.capitalize(fieldName);
-    }
-
-    private String setterMethodName(String fieldName) {
-        return "set" + StringUtils.capitalize(fieldName);
     }
 
     private <V> V asType(Object value, Class<V> type) {
         if (BigDecimal.class.equals(type)) {
             return (V) Utility.parseBigDecimal(value);
         } else if (Boolean.class.equals(type)) {
-            return (V) Utility.parseBoolean((String) value);
+            return (V) Utility.parseBoolean(value.toString());
         } else if (String.class.equals(type)) {
             return (V) Utility.parseString(value);
         } else if (Integer.class.equals(type)) {
