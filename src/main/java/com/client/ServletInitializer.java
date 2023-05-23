b854e2c390c8ccc1e1d9e2752c8ab3605a749127
Trevor Swartz <trevor.swartz@bullhorn.com>
2023-05-23 13:21:30 -0400

    Adding log4j api to allow for the use of LogManager + Changing main class to Application

--- src/main/java/com/client/ServletInitializer.java
+++ src/main/java/com/client/ServletInitializer.java
@@ -7,7 +7,7 @@ public class ServletInitializer extends SpringBootServletInitializer {
 
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(StarterKitSpringMaven.class);
+        return application.sources(Application.class);
     }
 
 }
